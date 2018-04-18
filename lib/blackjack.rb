80a1cc66e853acbaf07819c7351064a5d9af7840
Chris Alberti <christopherjalberti@gmail.com>
2018-04-18 18:41:40 +0000

    automatically backed up by learn

--- lib/blackjack.rb
+++ lib/blackjack.rb
@@ -18,7 +18,7 @@ def get_user_input
   input = gets.chomp
 end
 
-def end_game(total)
+def end_game
   puts "Sorry, your card total is #{display_card_total(total)}"
   puts "Thank you for playing"
 end
